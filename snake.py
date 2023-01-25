a800e00e5d17c8a45fd052317061e9733e13e7b8
venkata krishnaiah <pallavenkat1995@gmail.com>
2023-01-25 20:53:57 +0530

    enhanced

--- snake.py
+++ snake.py
@@ -1,96 +1,68 @@
 import pygame
 import time
-import random
 
-# Initialize pygame
+# Initialize pygame and create a window
 pygame.init()
-
-# Set the size of the window
 width = 500
 height = 500
+screen = pygame.display.set_mode((width, height))
 
-# Create the window
-win = pygame.display.set_mode((width, height))
+# Create a snake and food
+snake = [(200, 200), (210, 200), (220, 200)]
+food = (250, 250)
 
-# Set the title of the window
-pygame.display.set_caption("Snake Game")
-
-# Set the starting position of the snake
-x = 50
-y = 50
-
-# Set the size of each snake segment
-segment_width = 10
-segment_height = 10
-
-# Set the initial length of the snake
-length = 1
-
-# Set the initial direction of the snake
-x_change = 0
-y_change = 0
-
-# Set the initial speed of the snake
-speed = 10
-
-# Set the initial color of the snake
-color = (255, 0, 0)
-
-# Create a clock to control the speed of the game
-clock = pygame.time.Clock()
-
-# Create the food
-foodx = 0
-foody = 0
-food_color = (0, 255, 0)
-
-def create_food():
-    global foodx, foody
-    foodx = round(random.randrange(0, width - segment_width) / 10.0) * 10.0
-    foody = round(random.randrange(0, height - segment_height) / 10.0) * 10.0
+# Set initial score and movement direction
+score = 0
+direction = 'right'
 
 # Main game loop
 running = True
 while running:
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -segment_width
-                y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x_change = segment_width
-                y_change = 0
-            elif event.key == pygame.K_UP:
-                y_change = -segment_height
-                x_change = 0
-            elif event.key == pygame.K_DOWN:
-                y_change = segment_height
-                x_change = 0
 
-    # Move the snake
-    x += x_change
-    y += y_change
+    # Move the snake in the current direction
+    if direction == 'right':
+        snake[0] = (snake[0][0] + 10, snake[0][1])
+    elif direction == 'left':
+        snake[0] = (snake[0][0] - 10, snake[0][1])
+    elif direction == 'up':
+        snake[0] = (snake[0][0], snake[0][1] - 10)
+    elif direction == 'down':
+        snake[0] = (snake[0][0], snake[0][1] + 10)
 
-    # Clear the screen
-    win.fill((0, 0, 0))
+    # Check if the snake has hit the food
+    if snake[0][0] == food[0] and snake[0][1] == food[1]:
+        food = (snake[0][0]+10, snake[0][1]+10)
+        score += 1
 
-    # Draw the snake
-    pygame.draw.rect(win, color, (x, y, segment_width, segment_height))
+    # Move the rest of the snake
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
 
-    # Draw the food
-    pygame.draw.rect(win, food_color, (foodx, foody, segment_width, segment_height))
+    # Draw the snake and food
+    screen.fill((0, 0, 0))
+    for pos in snake:
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(food[0], food[1], 10, 10))
+    pygame.display.set_caption("Score: " + str(score))
+    pygame.display.flip()
 
-    # Update the screen
-    pygame.display.update()
+    # Handle key presses for movement
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        direction = 'left'
+    elif keys[pygame.K_RIGHT]:
+        direction = 'right'
+    elif keys[pygame.K_UP]:
+        direction = 'up'
+    elif keys[pygame.K_DOWN]:
+        direction = 'down'
 
-    # Check if the snake has collided with the food
-    if x == foodx and y == foody:
-        create_food()
+    # Wait for a bit
+    time.sleep(0.1)
 
-    # Set the speed of the game
-    clock.tick(speed)
-
-# Quit pygame
+# Exit the game
 pygame.quit()
